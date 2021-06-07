5960c7e5452119ac82136450e429b208c6773007
matiasnnr <matias.nnr@hotmail.com>
2021-06-07 14:36:15 -0400

    base, macros.html y herencia de templates

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Comprar café', 'Solicitud de compra', 'todo 3', 'todo 4']
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -16,5 +18,12 @@ def index():
 def hello():
     user_ip = request.cookies.get('user_ip')
     user_name = request.cookies.get('user_name')
+
+    context = {
+        'user_ip': user_ip, 
+        'user_name': user_name, 
+        'todos': todos
+    }
     # return f'Hola {user_name}, tu IP es {user_ip}'
-    return render_template('hello.html', user_ip=user_ip, user_name=user_name)
+    # **context hace que expanda y exponga todas las variables y no sea necesario usar contenxt.user_ip por ejemplo, simplemente llamamos a user_ip
+    return render_template('hello.html', **context)
