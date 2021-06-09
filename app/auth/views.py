512a8aabee65c3dd9ca5fc6047333b7dc13cf80e
matiasnnr <matias.nnr@hotmail.com>
2021-06-09 18:02:44 -0400

    add: signup

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,12 @@
 from flask_login.utils import login_required, login_user, current_user, logout_user
+from werkzeug.security import generate_password_hash
 from app.models import UserData, UserModel
 from flask import render_template, flash, redirect, url_for
 
 from app.forms import LoginForm
 
 from . import auth
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -44,6 +45,37 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form': signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+
+            login_user(user)
+
+            flash('Bienvenido!')
+
+            return redirect(url_for('hello'))
+
+        else:
+            flash('El usario existe!')
+
+    return render_template('signup.html', **context)
 
 @auth.route('/logout')
 @login_required
