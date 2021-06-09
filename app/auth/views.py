268aed764553ed55c80e59e38f1d8522fb1e2a33
matiasnnr <matias.nnr@hotmail.com>
2021-06-09 16:10:05 -0400

    add: login and logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,11 @@
-from flask import render_template, session, flash, redirect, url_for
+from flask_login.utils import login_required, login_user, current_user, logout_user
+from app.models import UserData, UserModel
+from flask import render_template, flash, redirect, url_for
 
 from app.forms import LoginForm
 
 from . import auth
+from app.firestore_service import get_user
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -12,12 +15,40 @@ def login():
         'login_form': login_form
     }
 
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
 
-        flash('Nombre de usuario registrado con éxito')
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido de nuevo!')
+
+                redirect(url_for('hello'))
+            else:
+                flash('El usuario o contraseña son incorrectos')
+        else:
+                flash('El usuario no existe')
 
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa pronto')
+
+    return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
 
+from flask_login import login_required, current_user
+
 from app import create_app
 from app.forms import LoginForm
 from app.firestore_service import get_users, get_todos
@@ -32,9 +34,10 @@ def index():
     return response
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip': user_ip, 
